ac277a00fb3eaf5de87c6c8638a9f94715daf769
Iasmim Torres <iasmimt31@gmail.com>
2023-01-28 12:51:37 -0300

    projeto

---
+++ src/main/java/br/com/springboot/model/User.java
@@ -0,0 +1,22 @@
+package br.com.springboot.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String username;
+
+}
